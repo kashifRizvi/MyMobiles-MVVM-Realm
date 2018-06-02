a33e7afd59f9b7e748f43b86aeda74b40e46d7ab
Kashif Rizvi <syed.rizvi@care.com>
2018-06-02 19:16:30 +0530

    data layer inclusion

---
+++ MyMobiles/Services/DataService.swift
@@ -0,0 +1,23 @@
+//
+//  DataService.swift
+//  MyMobiles
+//
+//  Created by Kashif Rizvi on 02/06/18.
+//  Copyright © 2018 Kashif Rizvi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class DataService {
+    
+    private var realm: Realm!
+    
+    init() {
+        realm = try! Realm()
+    }
+    
+    func fetchMobiles() -> Results<Mobile> {
+        return realm.objects(Mobile.self)
+    }
+}
